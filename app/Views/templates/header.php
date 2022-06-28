557bd60671d48e38673206c0e679416ec162fc16
DevLogsan <loganlegallou22@gmail.com>
2022-06-28 14:24:16 +0200

    header bootstrap

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -10,7 +10,7 @@
 <body>
 <header class="navbar navbar-expand-lg navbar-dark bd-navbar sticky-top" style="background-color: #043e6b;">
     <nav class="container-xxl bd-gutter flex-wrap flex-lg-nowrap">
-        <a class="navbar-brand p-0 me-0 me-lg-2" href="<?php echo site_url('Agences/accueil') ?>">
+        <a class="navbar-brand p-0 me-0 me-lg-2" href="<?php echo site_url('Agences/accueil') ?>" title="SC Micro">
             <img alt="sc-micro" height="50" src="<?php echo img_url('sc-micro.jpg')?>">
         </a>
         <div class="offcanvas-lg offcanvas-end flex-grow-1">
@@ -24,6 +24,17 @@
                     </li>
                     <li class="nav-item col-6 col-lg-auto">
                         <a class="nav-link py-2 px-0 px-lg-2 " href="<?php echo site_url('Agences/liste_des_agences') ?>">Liste des agences</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav flex-row flex-wrap ms-md-auto">
+                    <li class="nav-item col-6 col-lg-auto">
+                        <a class="nav-link py-2 px-0 px-lg-2" href="https://jaime.jlogiciels.fr">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-briefcase-fill" viewBox="0 0 16 16">
+                                <title>jlogiciels</title>
+                                <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v1.384l7.614 2.03a1.5 1.5 0 0 0 .772 0L16 5.884V4.5A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1h-3zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5z"/>
+                                <path d="M0 12.5A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5V6.85L8.129 8.947a.5.5 0 0 1-.258 0L0 6.85v5.65z"/>
+                            </svg>
+                        </a>
                     </li>
                 </ul>
             </div>
